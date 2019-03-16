4e49ed6713d527fcd11edc23111102021783a732
Igor Valnev <dx2389@gmail.com>
2019-03-16 16:58:12 +0300

    Сделал модальное окно + JS

---
+++ courseProject/src/js/modal.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".header-phone__button").click(function() {
+        $(".over").css("display", "flex")
+    })
+    $(".modal-window-close").click(function() {
+        $(".over").css("display", "none")
+    })
+})

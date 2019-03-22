bc5f58b269a9694f7aaf83212b9d8b9029c883b1
Igor Valnev <dx2389@gmail.com>
2019-03-22 16:38:45 +0300

    Начал делать слайдер на JS

---
+++ courseProject/src/js/feedback-slider.js
@@ -0,0 +1,20 @@
+// План работы:
+// 1. Находим контейнер слайдера.
+// 2. Находим кнопки правую и левую.
+// 3. Находим массив картинок.
+// 4. Через функцию по нажатию на кнопку меняем адрес 
+//     первых трёх картинок.
+var container = document.querySelector("#feedback-img-container")
+var left = document.querySelector(".fa-angle-left")
+var right = document.querySelector(".fa-angle-right")
+
+var img = container.querySelectorAll("img")
+
+
+
+
+
+
+
+
+console.log(img)

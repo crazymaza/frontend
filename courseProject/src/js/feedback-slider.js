123f1de96b8d990c840e5ac1f1ff604714c28a79
Igor Valnev <dx2389@gmail.com>
2019-03-23 00:36:34 +0300

    Пытался сделать слайдер для отзывов.

--- courseProject/src/js/feedback-slider.js
+++ courseProject/src/js/feedback-slider.js
@@ -9,6 +9,39 @@ var left = document.querySelector(".fa-angle-left")
 var right = document.querySelector(".fa-angle-right")
 
 var img = container.querySelectorAll("img")
+var imgContainer = container.querySelector(".img-container")
+
+
+index = 0
+
+left.addEventListener('click', function() {
+    if (index < 4) {
+        var currect = img[index]
+        var newImg = document.createElement('img')
+        img[index].style.transform = 'translateX(-290px)'  
+        img[index].remove()
+        currect.style.transform = 'translateX(0)'
+        imgContainer.insertAdjacentElement('beforeend', newImg)
+        index++
+        newImg.src = 'img/letter' + index + '.png'
+    }
+})
+
+right.addEventListener('click', function() {
+    if (index == 0) {
+        index = 4 
+    }
+    if (index > 0) {
+        index--
+        var currect = img[index]
+        var newImg = document.createElement('img')
+        img[index].style.transform = 'translateX(290px)'  
+        img[index].remove()
+        currect.style.transform = 'translateX(0)'
+        imgContainer.insertAdjacentElement('afterbegin', newImg)
+        newImg.src = 'img/letter' + (index + 1) + '.png'
+    }
+})
 
 
 
@@ -17,4 +50,12 @@ var img = container.querySelectorAll("img")
 
 
 
-console.log(img)
+
+
+
+
+console.log(imgContainer)
+
+function newFunction() {
+    imgContainer.appendChild(addNewImg);
+}
